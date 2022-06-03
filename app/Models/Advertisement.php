64b18084fb7e45fbc32d11ce251daee60fc6dc03
Mahmoud Mahmoud <54012843+Mahmoud217TR@users.noreply.github.com>
2022-06-03 12:08:08 +0300

    configured models

--- app/Models/Advertisement.php
+++ app/Models/Advertisement.php
@@ -2,10 +2,51 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Advertisement extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'description',
+        'state',
+        'user_id',
+    ];
+
+    protected $attributes = [
+        'state' => 1,
+    ];
+
+    public static function getStates(){
+        return [
+            1 => 'draft',
+            2 => 'published',
+        ];
+    }
+    
+    // The Accessor & Mutator
+    public function state(): Attribute{
+        return Attribute::make(
+            get: fn ($value) => $this->getStates()[$value],
+            set: fn ($value) => array_search($value,$this->getStates()),
+        );
+    }
+
+    // Relations
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+
+    // Scopes
+    public function scopeDraft($query){
+        return $query->where('state','1');
+    }
+    
+    public function scopePublished($query){
+        return $query->where('state','2');
+    }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -21,6 +22,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'role',
     ];
 
     /**
@@ -41,4 +43,41 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    protected $attributes = [
+        'role' => 1,
+    ];
+
+    public static function getRoles(){
+        return [
+            1 => 'user',
+            2 => 'admin',
+        ];
+    }
+
+    public function role(): Attribute{
+        return Attribute::make(
+            get: fn($value) => $this->getRoles()[$value],
+            set: fn($value) => array_search($value,$this->getRoles()),
+        );
+    }
+
+    // Relations
+    public function advertisements(){
+        return $this->hasMany(Advertisement::class);
+    }
+
+    // Scopes
+    public function scopeUser($query){
+        return $query->where('role','1');
+    }
+
+    public function scopeAdmin($query){
+        return $query->where('role','2');
+    }
+
+    // Functions
+    public function isOwner($object){
+        return $this->id == $object->user_id;
+    }
 }
