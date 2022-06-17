4a4f34ad908f26ccb941fe270ef561150d404a48
Mahmoud Mahmoud <54012843+Mahmoud217TR@users.noreply.github.com>
2022-06-17 14:36:28 +0300

    category factory fix

--- database/factories/CategoryFactory.php
+++ database/factories/CategoryFactory.php
@@ -17,7 +17,7 @@ class CategoryFactory extends Factory
     public function definition()
     {
         return [
-            'name' => $this->faker->sentence(1),
+            'name' => $this->faker->unique()->sentence(2),
             'description' => $this->faker->paragraph(),
         ];
     }
