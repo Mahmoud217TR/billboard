39b758ecce3e0a072808e6eb7fa2e79bdd6eddaa
Mahmoud Mahmoud <54012843+Mahmoud217TR@users.noreply.github.com>
2022-06-18 11:48:52 +0300

    added category name to search attribute

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -8,9 +8,15 @@ use Illuminate\Http\Request;
 
 class SearchController extends Controller
 {
+
     public function indexSearch(SearchRequest $request){
-        $advertisements = Advertisement::featuredFirst()->published()->latest()->where('title','like','%'.$request['keyword'].'%')
-        ->orWhere('description','like','%'.$request['keyword'].'%')->paginate(18);
+        $advertisements = Advertisement::featuredFirst()->published()->latest()
+        ->where('title', 'like', '%'.$request['keyword'].'%')
+        ->orWhere('description', 'like', '%'.$request['keyword'].'%')
+        ->orWhereHas('category',function($query) use ($request){
+            $query->where('name', 'like', '%'.$request['keyword'].'%');
+        })
+        ->paginate(18);
         return view('advertisement.results',compact('advertisements'));
     }
 }
